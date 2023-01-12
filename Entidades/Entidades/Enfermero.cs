cdd0c236416f97d7272050fa059af4e3a7bb6df1
Anais Moreno <anabet.94@gmail.com>
2023-01-12 17:51:09 -0300

    inicio

---
+++ Entidades/Entidades/Enfermero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Entidades
+{
+    public class Enfermero:Persona
+    {
+        public int Cuil { get; set; }
+    }
+}
---
+++ Entidades/Entidades/Medico.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Entidades
+{
+    public class Medico:Persona
+    {
+        public string Matricula { get; set; }
+        public string Epecialidad { get; set; }
+    }
+}
